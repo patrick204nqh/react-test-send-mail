9388c7650156bfa9a8a7cb7799b111e2989e1a37
patrick <patrick204nqh@gmail.com>
2020-08-01 12:44:54 +0700

    handle with input

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,33 @@ import { Form, FormGroup, Input, Label, Button } from 'reactstrap';
 
 
 class App extends React.Component {
-  handleChange = () => {
+  constructor() {
+    super()
 
+    this.state = {
+      name: '',
+      email: '',
+      message: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    const { name, email, message } = this.state;
   }
 
   render() {
     return (
-      <Form>
+      <Form style={{ width: '600px' }} onSubmit={this.handleSubmit}>
         <FormGroup>
           <Label for="name">Name:</Label>
           <Input
