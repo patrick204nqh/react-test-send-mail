3d463e66113ce266e6a66d9b3c615edee6b6b77f
patrick <patrick204nqh@gmail.com>
2020-08-01 13:03:43 +0700

    config scripts to test server

---
+++ README.md
@@ -0,0 +1,5 @@
+# Patrick's Project ( using React, reactstrap)
+
+1. Source video:
+
+> https://www.youtube.com/watch?v=EPnBO8HgyRU
--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Form, FormGroup, Input, Label, Button } from 'reactstrap';
-
-
+import axios from 'axios';
 
 class App extends React.Component {
   constructor() {
@@ -13,7 +12,8 @@ class App extends React.Component {
       message: ''
     }
 
-    this.handleChange = this.handleChange.bind(this)
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange = e => {
@@ -22,10 +22,16 @@ class App extends React.Component {
     })
   }
 
-  handleSubmit(e) {
+  async handleSubmit(e) {
     e.preventDefault();
 
     const { name, email, message } = this.state;
+
+    const form = await axios.post('/api/form', {
+      name,
+      email,
+      message
+    })
   }
 
   render() {
